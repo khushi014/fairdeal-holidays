d0395643a55722bbfbd928ec85ac155f737281ee
khushiAdosphere <khushi@adosphereindia.com>
2026-04-25 00:33:33 +0530

    testimonials added

--- js/testimonials.js
+++ js/testimonials.js
@@ -14,10 +14,18 @@ function loadServices() {
       title: "Subba Rao Ganti",
       desc: "We have been availing the services of Fairdeal for the past 20 years on various occasions. Fairdeal offers exceptionally high quality professional services with a touch of love and affection. Their prompt response to address any issues during ticketing, travel etc., is commendable. My best wishes to Fairdeal in providing great services to their customers.",
     },
-    // {
-    //   title: "Veerabhadra Sarma Ganti",
-    //   desc: "Fairdeal is committed to customer service. They provide five star service",
-    // },
+    {
+      title: "Foram Shah",
+      desc: "I had a wonderful experience with Mr. Ajay from Fairdeal holidays. The entire trip was well-organized. They were very responsive, provided clear information, and handled everything smoothly. The itinerary was thoughtfully planned, and all arrangements were comfortable and hassle-free. I would definitely recommend their services to anyone looking for a reliable travel guidance and bookings.",
+    },
+    {
+      title: "Sandip Parikh",
+      desc: "Great service from the first call to the end of the trip. Organized well with every detail without flaw. Also, communicated during the trip to make sure all was well. Very happy with chosen hotels, guides and transportation. Will use services again. Highly recommended!!😀",
+    },
+    {
+      title: "Shobna Narang",
+      desc: "3 days holiday to Bhuj was very well planned by Fairdeal experiencing road to heaven, watching museums of earthquake disaster, going back to history of Indus valley civilisation, experiencing camel carts and walking on salt lake. Very memorable trip. Very comfortable stay in Rann Utsav tent city with excellent hospitality.",
+    },
   ];
 
   testimonials.forEach((testimonial) => {
